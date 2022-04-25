7eae714b69250ca6bcf2c9f14986e62ca0e8bbe7
Tanvi Inamdar <104395891+InamdarT@users.noreply.github.com>
2022-04-25 16:35:49 -0700

    Update streamlit_app.py
    Added streamlit.title in front

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom\'s New Healthy Diner
+streamlit.title('My Mom\'s New Healthy Diner')
                 
-                Breakfast Menu
-                Omega 3 & Blueberry Oatmeal')
+streamlit.title('Breakfast Menu')
+streamlit.title ('Omega 3 & Blueberry Oatmeal')
